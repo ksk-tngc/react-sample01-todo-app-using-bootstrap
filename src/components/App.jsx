e83aba66fa37a3b8d22f07b708bd7461ed23b20d
ksk-tngc <59589496+ksk-tngc@users.noreply.github.com>
2021-01-26 17:47:38 +0900

    feat: テンプレート作成＆Bootstrap導入

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// React
+import React from 'react'
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="display-4">Hello</h1>
+    </>
+  )
+}
